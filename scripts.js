b376cea1196aab8e8d3331c5222453bf52f2631d
annajag <annajagiela@MacBook-Air-Anna.local>
2016-09-29 03:55:49 +0200

    8.8

---
+++ scripts.js
@@ -0,0 +1,5 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var zieloneSlonie = 'Zielone słonie'
+var change = text.replace('Papugi' , zieloneSlonie.toUpperCase('Zielone słonie'));
+console.log(change);
+console.log(change.substr(change.length/2));
